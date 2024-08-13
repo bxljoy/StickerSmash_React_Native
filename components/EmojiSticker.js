204076d7b7fef240e5ab5e095b0837024fc5a128
bxljoy <259500313@qq.com>
2024-08-13 03:51:55 +0200

    adding pan gesture

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -13,6 +13,8 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
     .onStart(() => {
       if (scaleImage.value !== imageSize * 2) {
         scaleImage.value = scaleImage.value * 2;
+      } else {
+        scaleImage.value = imageSize;
       }
     });
 
@@ -23,15 +25,38 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
     };
   });
 
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const drag = Gesture.Pan().onChange((event) => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
   return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
